d6aed0a17d3db59b7c71d5af54214e547e650c38
Chemex <chemex@MacBook-Pro-de-Chemex.local>
2025-08-25 15:59:04 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'los_estafadores_app',
+  webDir: 'dist'
+};
+
+export default config;
